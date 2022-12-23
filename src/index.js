b35ffe001dac70762a34d586f704a1af819d202b
Sara <sara.sarabii93@gmail.com>
2022-12-24 00:31:06 +0330

    search updated

--- src/index.js
+++ src/index.js
@@ -41,21 +41,40 @@ function displayTemperature(response) {
   );
 }
 
-let dateElement = document.querySelector("#date");
-dateElement.innerHTML = "HEYYYYS";
-let apiKey = "57e196ced6754eafbd1165545222212";
-let apiUrl = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=karaj`;
-axios.get(apiUrl).then(displayTemperature);
+function displayIcon(response) {
+  let iconElement = document.querySelector("#icon");
+  iconElement.setAttribute("src", `${response.data.condition.icon_url}`);
+}
 
-//function displayForecast() {
-let forecastElement = document.querySelector("#forecast");
-let forecastHTML = `<div class="row">`;
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
 
-//let days = ["Thu", "Fri", "Sat", "Sun", "Mon", "Tue", "Wed"];
-//days.forEach(function (day) {
-// forecastHTML =
-// forecastHTML +
-`
+function search(city) {
+  let apiKey = "57e196ced6754eafbd1165545222212";
+  let apiUrl = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}`;
+  axios.get(apiUrl).then(displayTemperature);
+  let apiKey2 = "fta134003b032o583a14a7f723b18aa3";
+  let apiUrl2 = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey2}`;
+  axios.get(apiUrl2).then(displayIcon);
+}
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
+
+search("Tabriz");
+
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+  let forecastHTML = `<div class="row">`;
+
+  let days = ["Thu", "Fri", "Sat", "Sun", "Mon", "Tue", "Wed"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
        <div class="col">
               <div class="forecast-day">${day}</div>
               <div class="forecast-icon">
@@ -69,9 +88,10 @@ let forecastHTML = `<div class="row">`;
                 <span class="forecats-temperature-min">12°</span>
               </div>
             </div>`;
-//});
+  });
 
-//forecastHTML = forecastHTML + `</div>`;
-//forecastElement.innerHTML = forecastHTML;
-//}
-//displayForecast();
+  forecastHTML = forecastHTML + `</div>`;
+
+  forecastElement.innerHTML = forecastHTML;
+}
+displayForecast();
